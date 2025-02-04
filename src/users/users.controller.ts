cc1a025a8b8d4681f6971bff975cf6a29813761f
Joahan3250 <joahan.sanchez@alogasoftwaresolution.com>
2025-02-04 15:34:13 -0600

    CRUD finalizado

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get } from "@nestjs/common";
+import { Controller, Post, Body,Param, Get, ParseIntPipe, Delete, Patch } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { UpdateUserDto } from "./dto/update-user.dto";
 @Controller('users')
 export class UsersController{
     constructor(private usersService: UsersService){}//esto sirve por que tambien debo de crear lo del otro controlador (donde mando a crear el usuario);
@@ -10,9 +11,24 @@ export class UsersController{
 getUsers(): Promise<User[]>{
     return this.usersService.getUsers();
 }
+
+@Get(':id')
+
+ getUser(@Param('id',ParseIntPipe)id: number): Promise<User>{
+ return this.usersService.getUser(id);
+}
+
     @Post()
 createUser(@Body() newUser: CreateUserDto ){//El decorador Body son los datos que vienen desde el cliente 
 return this.usersService.createUser(newUser)//Esto se hace para poder usarlo
 } 
 
+@Delete(':id')
+deleteUser(@Param('id',ParseIntPipe)id: number){
+return this.usersService.deleteUser(id);
+}
+@Patch(':id')
+updateUser(@Param('id',ParseIntPipe) id:number, @Body()user: UpdateUserDto){
+    return this.usersService.updateUser(id,user)
+}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private userRepository: Repository<User>){ // Este constructor le debemos de mandar primero el decorador,a su vez ese decorador pide un parametro, el cual es la entidad de la tabla que creamos, posteriormente debemos de agregarle un nombre a esta entidad. Posterior a ello debemos de importar la biblioteca "Repository" y posterior a ello indicarle al nombre que es de tipo "Repository" y al tipo de datos le decimos que es de tipo usuario para que funcione y para que se instancie le agregamos un private
@@ -16,4 +17,23 @@ export class UsersService {
     getUsers(){
         return this.userRepository.find()
     }
-} 
+
+    getUser(id: number): Promise<User> {
+        return this.userRepository.findOne({ where: { id } }).then(user => {
+            if (!user) {
+                throw new NotFoundException(`User with ID ${id} not found`);
+            }
+            return user;
+        });
+    }
+
+deleteUser(id: number){
+    return this.userRepository.delete({id})
+}
+
+updateUser(id: number, user: UpdateUserDto){
+    return this.userRepository.update({id}, user)
+}
+
+    }
+
